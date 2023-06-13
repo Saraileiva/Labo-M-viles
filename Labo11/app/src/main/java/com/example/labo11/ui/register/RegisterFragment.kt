dcea7f40decea4b3b654be02253b08c4705e85a6
Saraileiva <00164021@uca.edu.sv>
2023-06-13 17:30:55 -0600

    Labo11

---
+++ Labo11/app/src/main/java/com/example/labo11/ui/register/RegisterFragment.kt
@@ -0,0 +1,72 @@
+package com.example.labo11.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.labo11.R
+import com.example.labo11.RetrofitApplication
+import com.example.labo11.databinding.FragmentRegisterBinding
+import com.example.labo11.ui.register.viewmodel.RegisterViewModel
+
+class RegisterFragment : Fragment() {
+
+    private val registerViewModel: RegisterViewModel by activityViewModels {
+        RegisterViewModel.Factory
+    }
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    val app by lazy {
+        requireActivity().application as RetrofitApplication
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViewModel()
+        observeStatus()
+    }
+
+    private fun setViewModel() {
+        binding.viewmodel = registerViewModel
+    }
+
+    private fun observeStatus() {
+        registerViewModel.status.observe(viewLifecycleOwner) { status ->
+            handleUiStatus(status)
+        }
+    }
+
+    private fun handleUiStatus(status: RegisterUiStatus) {
+        when (status) {
+            is RegisterUiStatus.Error -> {
+                Toast.makeText(requireContext(), "An error has occurred", Toast.LENGTH_SHORT).show()
+            }
+
+            is RegisterUiStatus.ErrorWithMessage -> {
+                Toast.makeText(requireContext(), status.message, Toast.LENGTH_SHORT).show()
+            }
+
+            is RegisterUiStatus.Success -> {
+                registerViewModel.clearStatus()
+                registerViewModel.clearData()
+                //app.saveAuthToken(status)
+                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+
+            else -> {}
+        }
+    }
+}
